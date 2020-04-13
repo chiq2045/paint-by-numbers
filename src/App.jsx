b56cae3c812e899d5a1421a72307c8cd8cd206f3
chiq2045 <charles.oroko@outlook.com>
2020-04-13 16:01:21 +0000

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
-import UploadForm from './views/UploadForm';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import Login from './views/Login';
+import NavBar from './components/NavBar';
+import UserDashBoard from './views/user/Dashboard';
+import AdminDashboard from './views/admin/Dashboard';
+import Logout from './views/Logout';
 
 export default function App() {
   return (
-    <UploadForm />
+    <div>
+      <Router>
+        <Switch>
+          <Route
+            exact
+            path='/'
+            component={Login}
+          />
+          <Route path='/user'>
+            <NavBar isAdmin={false} />
+            <UserDashBoard />
+          </Route>
+          <Route path='/admin'>
+            <NavBar isAdmin={true} />
+            <AdminDashboard />
+          </Route>
+          <Route path='/logout' component={Logout} />
+        </Switch>
+      </Router>
+    </div>
   );
 }
