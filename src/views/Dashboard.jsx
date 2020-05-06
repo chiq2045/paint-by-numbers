3da2db47889fbd09f1eaa5271e05b78e7fbbde8b
chiq2045 <charles.oroko@outlook.com>
2020-05-05 17:39:54 -0700

    update Dashboard message

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Dashboard() {
   return (
-    <div>
+    <div className='content'>
       <h4>Welcome to Paint by Numbers</h4>
       <p>This is a web app that gives students a method to color images by section.
         Each section has a number and can be filled in with a color of choice by clicking on said section.</p>
