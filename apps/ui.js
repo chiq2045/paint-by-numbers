9624e1c963e37893d3a486f18f436b277bd45fac
chiq2045 <charles.oroko@outlook.com>
2020-05-06 02:53:19 +0000

    remove backend from project

--- apps/ui.js
+++
@@ -1,9 +0,0 @@
-const Koa = require('koa');
-const serve = require('koa-static');
-const { resolve } = require('path');
-
-const ui = new Koa();
-
-ui.use(serve(resolve(__dirname, '../dist')));
-
-module.exports = ui;
