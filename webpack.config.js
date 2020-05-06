544d423093c1b0e8509fc5cde9178ac946d0caa9
chiq2045 <charles.oroko@outlook.com>
2020-05-05 17:37:38 -0700

    update webpackDevServer to resolve 404s

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,8 @@ module.exports = {
   devServer: {
     contentBase: resolve(__dirname, 'dist'),
     compress: true,
-    port: 9000
+    port: 9000,
+    historyApiFallback: true
   },
   module: {
     rules: [
