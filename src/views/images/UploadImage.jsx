35fadf399da902c591aa69eea2f513c0d229bc38
chiq2045 <charles.oroko@outlook.com>
2020-05-19 20:21:40 +0000

    add new function to upload image

--- src/views/images/UploadImage.jsx
+++ src/views/images/UploadImage.jsx
@@ -1,10 +1,12 @@
 import React, { useCallback, useState } from 'react';
+import { Redirect } from 'react-router-dom';
 import { useDropzone } from 'react-dropzone';
 import axios from 'axios';
 import { useAuth0 } from '../../utils/react-auth0-spa';
 
 export default function UploadImage() {
   const [files, setFiles] = useState([]);
+  const [uploaded, setUploaded] = useState(false);
   const { getTokenSilently } = useAuth0();
 
   const onDrop = useCallback(acceptedFiles => {
@@ -25,16 +27,23 @@ export default function UploadImage() {
       const token = await getTokenSilently();
       const instance = axios.create({
         baseURL: 'https://api.ogamba.com/paint/private',
-        headers: { Authorization: `Bearer ${token}` }
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Access-Control-Allow-Origin': 'https://api.ogamba.com'
+        }
       });
       const response = await instance.post('/upload/single', data);
-
-      const responseData = await response.json();
-
-      console.log(responseData);
+      console.log(response.data);
+      setUploaded(true);
     } catch (error) {
       console.error(error);
+      setUploaded(false);
     }
+  };
+
+  const resetUpload = () => {
+    setUploaded(false);
+    setFiles([]);
   };
 
   const fileData = () => {
@@ -50,20 +59,76 @@ export default function UploadImage() {
     }
   };
 
+  // const RenderUploadPrompt = () => {
+  //   return (
+  //     <div className='content'>
+  //       <div className='dropzone' {...getRootProps()}>
+  //         <input className='col-9' {...getInputProps()} />
+  //         {
+  //           isDragActive
+  //             ? <p>Drop the file here...</p>
+  //             : <p>Drag n&apos; Drop a file here, or click here to select file</p>
+  //         }
+  //       </div>
+  //       <div className='btn-group'>
+  //         <button className='btn btn-primary animated' onClick={handleUpload}>Upload</button>
+  //         <button className='btn btn-dark animated' onClick={resetUpload}>Reset File</button>
+  //       </div>
+  //       {fileData()}
+  //     </div>
+  //   );
+  // };
+
+  // const CompleteUploadPrompt = () => {
+  //   return (
+  //     <div className='content'>
+  //       <div className='btn-group'>
+  //         <button className='btn-dark' onClick={resetUpload}>Upload Another</button>
+  //         <button className='btn-dark'>Done Uploading</button>
+  //       </div>
+  //     </div>
+  //   );
+  // };
+
   return (
     <div>
-      <div className='row ignore-screen level dropzone' {...getRootProps()}>
-        <input className='col-9 ignore-screen level-item' {...getInputProps()} />
-        {
-          isDragActive
-            ? <p>Drop the files here...</p>
-            : <p>Drag n&apos; Drop some files here, or click here to select files</p>
-        }
+      <div className='content'>
+        <h5>Upload Image</h5>
+        <p>You can upload an image by clicking the prompt below, or draggin a file onto it.</p>
+        <p>The app accepts the following file types</p>
+        <ul>
+          <li>png</li>
+          <li>jpg/jpeg</li>
+          <li>gif</li>
+          <li>bmp</li>
+          <li>svg</li>
+        </ul>
       </div>
-      <div className='row ignore-screen level-item btn-container'>
-        <button className='btn btn-primary animated' onClick={handleUpload}>Upload</button>
-      </div>
-      {fileData()}
+      {uploaded && (
+        <div className='content'>
+          <div className='btn-group'>
+            <button className='btn-dark' onClick={resetUpload}>Upload Another</button>
+            <button className='btn-dark' onClick={() => <Redirect to='/admin/images' />}>Done Uploading</button>
+          </div>
+        </div>
+      )}
+      {!uploaded && (
+        <div className='content'>
+          <div className='dropzone' {...getRootProps()}>
+            <input className='col-9' {...getInputProps()} />
+            {
+              isDragActive
+                ? <p>Drop the file here...</p>
+                : <p>Drag n&apos; Drop a file here, or click here to select file</p>
+            }
+          </div>
+          <div className='btn-group'>
+            <button className='btn btn-primary animated' onClick={handleUpload}>Upload</button>
+            <button className='btn btn-dark animated' onClick={resetUpload}>Reset File</button>
+          </div>
+          {fileData()}
+        </div>
+      )}
     </div>
   );
 }
