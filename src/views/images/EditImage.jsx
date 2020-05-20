e863d431ca27d3043654e7aff3aea5aa4df07c53
chiq2045 <charles.oroko@outlook.com>
2020-05-20 23:23:26 +0000

    changes paths to black on click

--- src/views/images/EditImage.jsx
+++ src/views/images/EditImage.jsx
@@ -1,9 +1,14 @@
 /* eslint-disable react/prop-types */
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { useAuth0 } from '../../utils/react-auth0-spa';
 
 export default function EditImage(props) {
   const { images, loaded } = props;
+  const [currentFill, setCurrentFill] = useState('');
+  // const [svgPaths, setSvgPaths] = useState([]);
+  const { getTokenSilently } = useAuth0();
   // const [svg, setSvg] = useState([]);
   // const [found, setFound] = useState(false);
   const { id } = useParams();
@@ -18,16 +23,26 @@ export default function EditImage(props) {
   //   };
   // }
   const handleChangeColor = pathId => {
+    const tempSvgPaths = svgPaths;
+    tempSvgPaths[pathId-1].fill = color;
+    setSvgPaths(tempSvgPaths);
     document.getElementById(pathId).setAttribute('fill', 'black');
-  }
+  };
+  const handleMouseOver = pathId => {
+    setCurrentFill(document.getElementById(pathId).getAttribute('fill'));
+    document.getElementById(pathId).setAttribute('fill', 'lightgray');
+  };
+  const handleMouseOut = pathId => {
+    document.getElementById(pathId).setAttribute('fill', currentFill);
+  };
 
   const RenderSvg = () => {
     const svg = images[parseInt(id-1)];
     const svgData = JSON.parse(svg.value);
     const { height, width } = svgData[0];
-    const svgPaths = svgData.slice(1);
     const paths = [];
-    // eslint-disable-next-line
+    const svgPaths = svgData.slice()
+    // setSvgPaths(svgData.slice(1));
     svgPaths.forEach(path => {
       paths.push(<path 
         key={`path-${path.id}`}
@@ -36,7 +51,9 @@ export default function EditImage(props) {
         fill={path.fill}
         stroke={path.stroke}
         fillRule={path.fillRule}
-        // onClick={handleChangeColor(path.id)}
+        onMouseOut={() => handleMouseOut(path.id)}
+        onMouseOver={() => handleMouseOver(path.id)}
+        onClick={() => handleChangeColor(path.id)}
       />,)
     });
     return (
@@ -50,16 +67,53 @@ export default function EditImage(props) {
     );
   };
 
-  return (
-    <div className='content'>
-      {
-        loaded
-          ? (
-            <object>
-              <RenderSvg />
-            </object>
-          ) : <div className='loader'>Loading...</div>
+  const handleClearChanges = () => {
+    setSvgPaths(JSON.parse(images[parseInt(id-1)].value.slice(1)));
+  };
+
+  const handleSaveChanges = async() => {
+    const token = await getTokenSilently();
+    const instance = axios.create({
+      baseURL: 'https://api.ogamba.com/paint/private',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Access-Control-Allow-Origin': 'https://api.ogamba.com'
       }
+    });
+    const svg = images[id-1];
+    const config = svg.value[0];
+    const data = [];
+    data.push(config);
+    svgPaths.forEach(path => {
+      data.push(path);
+    });
+    await instance.put(`/update/${id}`, data);
+  };
+  return (
+    <div>
+      <div className='content'>
+        <div className='row'>
+          <div className='col-4'>
+            <button className='btn-light' onClick={() => handleClearChanges}>Clear Changes</button>
+          </div>
+          <div className='col-4'>
+            <button className='btn-light' onClick={() => handleSaveChanges}>Save Changes</button>
+          </div>
+          <div className='col-4'>
+            <button className='btn-light'>Add to Assignment</button>
+          </div>
+        </div>
+      </div>
+      <div className='content'>
+        {
+          loaded
+            ? (
+              <object>
+                <RenderSvg />
+              </object>
+            ) : <div className='loader'>Loading...</div>
+        }
+      </div>
     </div>
   );
 }
