ad040a166276845cbf6db00e30884f39d4f0de31
chiq2045 <charles.oroko@outlook.com>
2020-05-27 19:23:47 +0000

    handle changes using state variables

--- src/views/images/EditImage.jsx
+++ src/views/images/EditImage.jsx
@@ -1,63 +1,96 @@
 /* eslint-disable react/prop-types */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import CirclePicker from './ColorPicker';
 import axios from 'axios';
 import { useAuth0 } from '../../utils/react-auth0-spa';
 
 export default function EditImage(props) {
   const { images, loaded } = props;
-  const [currentFill, setCurrentFill] = useState('');
+  const [color, setColor] = useState({ color: 'black', pathId: 0});
+  const [changeColor, setChangeColor] = useState(false);
+  // const [borders, setBorders] = useState(false);
+  // const [currentFill, setCurrentFill] = useState('');
   // const [svgPaths, setSvgPaths] = useState([]);
   const { getTokenSilently } = useAuth0();
-  // const [svg, setSvg] = useState([]);
+  const [readyToRender, setReadyToRender] = useState(false);
+  const [svg, setSvg] = useState({});
+  const [svgData, setSvgData] = useState({ config: {}, paths: [] });
+  const [strokeDasharray, setStrokeDasharray] = useState([]);
   // const [found, setFound] = useState(false);
   const { id } = useParams();
 
-  // const findImage = () => {
-  //   const foundImage = images.find(image => {
-  //     image.id == parseInt(id);
-  //   });
-  //   if (foundImage) {
-  //     setFound(true);
-  //     setSvg(foundImage);
-  //   };
-  // }
-  const handleChangeColor = pathId => {
-    const tempSvgPaths = svgPaths;
-    tempSvgPaths[pathId-1].fill = color;
-    setSvgPaths(tempSvgPaths);
-    document.getElementById(pathId).setAttribute('fill', 'black');
+  useEffect(() => {
+    if (loaded)
+      setSvg(images[parseInt(id-1)]);
+  }, [loaded]);
+
+  useEffect(() => {
+    if (svg.value) {
+      setSvgData({
+        config: svg.value[0],
+        paths: svg.value.slice(1)
+      });
+      setReadyToRender(true);
+    }
+  }, [svg]);
+
+  // useEffect(() => {
+  //   if (svgData.paths.length > 0) {
+  //     // const pathsArray = svgData.paths;
+  //     // pathsArray.forEach(path => {
+  //     //   setStrokeDasharray(strokeDasharray => [...strokeDasharray, '']);
+  //     // });
+  //   }
+  // }, [svgData]);
+
+  // useEffect(() => {
+  //   setChangeColor(false);
+  // }, [changeColor]);
+
+  const handleChangePathFill = pathId => {
+    const { paths, config } = svgData;
+    paths[pathId-1].fill = color.color;
+    setSvgData({ paths: paths, config: config });
   };
+
   const handleMouseOver = pathId => {
-    setCurrentFill(document.getElementById(pathId).getAttribute('fill'));
-    document.getElementById(pathId).setAttribute('fill', 'lightgray');
+    const dashArray = strokeDasharray;
+    dashArray[pathId-1] = 4;
+    setStrokeDasharray(dashArray);
   };
+
   const handleMouseOut = pathId => {
-    document.getElementById(pathId).setAttribute('fill', currentFill);
+    const dashArray = strokeDasharray;
+    dashArray[pathId-1] = '';
+    setStrokeDasharray(dashArray);
   };
 
   const RenderSvg = () => {
-    const svg = images[parseInt(id-1)];
-    const svgData = JSON.parse(svg.value);
-    const { height, width } = svgData[0];
+    const { height, width } = svgData.config;
     const paths = [];
-    const svgPaths = svgData.slice()
+    const svgPathsArray = svgData.paths;
     // setSvgPaths(svgData.slice(1));
-    svgPaths.forEach(path => {
+    svgPathsArray.forEach(path => {
+      const pathId = path.id;
       paths.push(<path 
-        key={`path-${path.id}`}
-        id={path.id}
+        key={`path-${pathId}`}
+        id={pathId}
         d={path.d}
         fill={path.fill}
         stroke={path.stroke}
         fillRule={path.fillRule}
-        onMouseOut={() => handleMouseOut(path.id)}
-        onMouseOver={() => handleMouseOver(path.id)}
-        onClick={() => handleChangeColor(path.id)}
+        onMouseOut={() => handleMouseOut(pathId)}
+        onMouseOver={() => handleMouseOver(pathId)}
+        strokeDasharray={strokeDasharray[pathId-1]}
+        onClick={() => handleChangePathFill(pathId)}
       />,)
     });
+
     return (
       <svg
+        id='svg'
+        name={svg.name}
         viewBox={`0 0 ${width} ${height}`}
         preserveAspectRatio='xMidYMin meet'
         height='100%'
